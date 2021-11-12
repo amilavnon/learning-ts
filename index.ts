ebd2d06b1c3efaf7d378da87ae54196b590b10cd
amilavnon <jdamilaspverinon@gmail.com>
2021-11-12 10:56:56 +0530

    create interface

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log('Start Type Script :)')
+// interfaces 
+interface IsPerson {
+    name: string;
+    age: number;
+    speak(a:string): void;
+    spend(a: number):number;
+}
+
+const somePerson: IsPerson = {
+    name: 'Amila',
+    age: 21,
+    speak(text:string): void {
+        console.log(text);
+    },
+    spend(amount:number):number {
+        console.log(amount);
+        return amount;
+    }    
+}
+
+const greedPreson = (person: IsPerson) => {
+    console.log(`Hello ${person.name}`)
+}
+
+greedPreson(somePerson)
