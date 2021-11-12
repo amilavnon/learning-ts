a9080271b69b48a043cf55b27da6f9131631f7a4
amilavnon <jdamilaspverinon@gmail.com>
2021-11-12 11:24:06 +0530

    refactor

--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@ interface IsPerson {
     speak(a:string): void;
     spend(a: number):number;
 }
-
 const somePerson: IsPerson = {
     name: 'Amila',
     age: 21,
@@ -18,10 +17,9 @@ const somePerson: IsPerson = {
         return amount;
     }    
 }
-
 const anotherPerson: IsPerson = {
-    name: 'Amila',
-    age: 21,
+    name: 'Sam',
+    age: 2,
     color: 'green',
     speak(text:string): void {
         console.log(text);
@@ -31,9 +29,28 @@ const anotherPerson: IsPerson = {
         return amount;
     }    
 }
-
 const greedPreson = (person: IsPerson) => {
     console.log(`Hello ${person.name}`)
 }
-
 greedPreson(somePerson)
+
+class Person implements IsPerson {
+    name: string
+    age: number
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
+    speak(text:string): void{
+        console.log(`voice: ${text}`)
+    }
+    spend(amount:number): number{
+        return amount
+    }
+}
+
+const person = new Person('amila', 20)
+
+person.speak(`I'm amila. Who are you ?`)
+const spendAmount = person.spend(20)
+console.log(`person spent : ${spendAmount}`)
