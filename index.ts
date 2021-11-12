ba6101558a438248cc1397813e4067da1ee6f636
amilavnon <jdamilaspverinon@gmail.com>
2021-11-12 11:20:19 +0530

    implement class from interface

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 interface IsPerson {
     name: string;
     age: number;
+    color?: string;
     speak(a:string): void;
     spend(a: number):number;
 }
@@ -9,6 +10,19 @@ interface IsPerson {
 const somePerson: IsPerson = {
     name: 'Amila',
     age: 21,
+    speak(text:string): void {
+        console.log(text);
+    },
+    spend(amount:number):number {
+        console.log(amount);
+        return amount;
+    }    
+}
+
+const anotherPerson: IsPerson = {
+    name: 'Amila',
+    age: 21,
+    color: 'green',
     speak(text:string): void {
         console.log(text);
     },
